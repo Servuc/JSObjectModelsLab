6d5285c8cb6676f200dfd5a85db02dbd46e4f98a
Thomas Servuc <thomas.selle@orange.fr>
2015-10-20 15:59:34 +0200

    Version pour Travis

--- tests/test.js
+++ tests/test.js
@@ -7,11 +7,7 @@
     /*---------------------------------*/
     /*     PART ONE: SIMPLE MODULE     */
     /*---------------------------------*/
-    module('SpeedCheck Module', {
-        beforeEach: function() {
-
-        }
-    });
+    module('SpeedCheck Module');
 
     // TODO: Vérifier que la création d'objets SpeedCheck est Possible
     test('Test objects creating', function() {
@@ -52,6 +48,9 @@
       equal(mySpeed.licencePlate, "BM108QE", "BM108QE licence plate without -");
       throws(function() { mySpeed.licencePlate = "123456"; }, "Invalid licence plate - 123456 - SHOULD throw a SpeedCheckError");
       equal(mySpeed.licencePlate, "BM108QE", "Last good licence plate without -");
+      throws(function() { mySpeed.licencePlate = "B108QE"; }, "Invalid licence plate - B108QE - SHOULD throw a SpeedCheckError");
+      throws(function() { mySpeed.licencePlate = "BM08QE"; }, "Invalid licence plate - BM08QE - SHOULD throw a SpeedCheckError");
+      throws(function() { mySpeed.licencePlate = "BM108Q"; }, "Invalid licence plate - BM108Q - SHOULD throw a SpeedCheckError");
     });
 
     // TODO: Vérifier que TOUTES les fonctionnalités de createSpeedCheckBE sont correctes (effects de bords, valeurs négatives, etc.) pour tous les attributs (speed et licencePlate)
@@ -72,6 +71,9 @@
       equal(mySpeed.licencePlate, "1-ABC-123", "1-ABC-123 licence plate");
       throws(function() { mySpeed.licencePlate = "123456"; }, "Invalid licence plate - 123456 - SHOULD throw a SpeedCheckError");
       equal(mySpeed.licencePlate, "1-ABC-123", "Last good licence plate");
+      throws(function() { mySpeed.licencePlate = "-ABC-123"; }, "Invalid licence plate - -ABC-123 - SHOULD throw a SpeedCheckError");
+      throws(function() { mySpeed.licencePlate = "1-AC-123"; }, "Invalid licence plate - 1-AC-123 - SHOULD throw a SpeedCheckError");
+      throws(function() { mySpeed.licencePlate = "1-ABC-23"; }, "Invalid licence plate - 1-ABC-23 - SHOULD throw a SpeedCheckError");
     });
 
     // TODO: Vérifier que la fonction toString() fonctionne bien.
@@ -90,196 +92,6 @@
       equal(mySpeed.toString(), "Véhicule 1-ABC-123 roule à 101 km/h. Ça va, circulez...", "BE : toString OKI (Véhicule 1-ABC-123 roule à 101 km/h. Ça va, circulez...)");
     });
 
-    /*---------------------------------*/
-    /*  PART TWO: The "Shapes" module  */
-    /*---------------------------------*/
-    var roadAttr, amenityAttr, buildingAttr, naturalAttr;
-    module('Unit Testing The "Shapes" Module', {
-      beforeEach: function(){
-        roadAttr = JSON.parse('{ \
-          "building": false, \
-          "highway": "residential", \
-          "_id": "-629863", \
-          "nodes": [{ \
-              "y": 369.0, \
-              "x": 708.0 \
-          }, { \
-              "y": 396.0, \
-              "x": 743.0 \
-          }], \
-          "name": "Rue de Colmar" \
-          } \
-        ');
 
-        amenityAttr = JSON.parse('{\
-        "_id":"-629724",\
-        "nodes":[{"y":32.0,\
-        "x":629.0},\
-        {"y":42.0,\
-        "x":597.0},\
-        {"y":43.0,\
-        "x":595.0},\
-        {"y":32.0,\
-        "x":629.0}],\
-        "amenity":"parking"}');
-
-        buildingAttr = JSON.parse('{"building":true,\
-        "_id":"-629719",\
-        "nodes":[{"y":0.0,\
-        "x":0.0},\
-        {"y":0.0,\
-        "x":100.0},\
-        {"y":100.0,\
-        "x":100.0},\
-        {"y":100.0,\
-        "x":0.0},\
-        {"y":0.0,\
-        "x":0.0}]}');
-
-        naturalAttr = JSON.parse('{"building":false,\
-        "_id":"-630043",\
-        "nodes":[{"y":309.0,\
-        "x":222.0},\
-        {"y":324.0,\
-        "x":262.0},\
-        {"y":335.0,\
-        "x":231.0},\
-        {"y":309.0,\
-        "x":222.0}],\
-        "name":"Bassin Paul Vatine",\
-        "natural":"water"}');
-
-      }
-    });
-
-
-    test('Test Shapes\' VERSION Property', function() {
-        expect(1);
-        equal(window.Shapes.VERSION, '0.0.1', 'The Shapes module should have a VERSION property');
-    });
-
-    test('Test Creation of a Default Object', function() {
-      expect(2);
-      ok(typeof window.Shapes.createShape !== 'undefined', 'The Shapes module sould expose a "createShape" function');
-      var shape0 = window.Shapes.createShape(roadAttr);
-      ok(typeof shape0 === 'object', 'The "createShape" function sould return objects.');
-    });
-
-    test('Test proper hidding of properties', function() {
-      expect(4);
-      var shape0 = window.Shapes.createShape(roadAttr);
-      var prop;
-      var props = [];
-      for(prop in shape0){
-        if(shape0.hasOwnProperty(prop)){
-          props.push(prop);
-        }
-      }
-      // Only 3 properties SHOULD be  visible
-      //{ id: [Function], toString: [Function], toSVGPath: [Function] }
-      equal(props.length, 3, 'Only 3 properties SHOULD be  visible in objects created by "createShape"');
-      for(prop in props){
-        ok(prop === 'id' || prop === 'toString' || prop === 'toSVGString', 'One of "id" "toString" or "toSVGString"');
-      }
-    });
-
-
-    test('Test the toSVGString method', function() {
-      expect(1);
-      var shape0 = window.Shapes.createShape(roadAttr);
-      equal(shape0.toSVGString(), 'M 708.0 369.0 L 743.0 396.0', 'Should create a valid SVG PATH (google SVG PATH for details)');
-    });
-
-    test('Test the name accessor', function() {
-      expect(1);
-      var shape0 = window.Shapes.createShape(roadAttr);
-      equal(shape0.getName(), 'Rue de Colmar', 'Should return the value corresponding to the "name" property in the attributes');
-    });
-
-
-    test('Test the createRoad function', function() {
-      expect(1);
-      ok(typeof window.Shapes.createRoad !== 'undefined', 'The Shapes module sould expose a "createRoad" function');
-    });
-
-    test('Test objects created with the createRoad function', function() {
-      expect(2);
-      var road = window.Shapes.createRoad(roadAttr);
-      ok(typeof road.getCaterogy === 'function', 'Object Created with "createRoad" Should have a getCategory function');
-      equal(road.getCaterogy(),'Residential', 'Should return the value corresponding to the "highway" property in the attributes');
-    });
-
-    test('Test the createAmenity function', function() {
-      expect(1);
-      ok(typeof window.Shapes.createAmenity !== 'undefined', 'The Shapes module sould expose a "createAmenity" function');
-    });
-    test('Test objects created with the  createAmenity function', function() {
-      expect(2);
-      var amenity = window.Shapes.createAmenity(amenityAttr);
-      ok(typeof amenity.getType === 'function', 'Object Created with "createAmenity" Should have a getType function');
-      equal(amenity.getType(),'parking', 'Should return the value corresponding to the "amenity" property in the attributes');
-    });
-
-    test('Test the createBuilding function', function() {
-      expect(1);
-      ok(typeof window.Shapes.createBuilding !== 'undefined', 'The Shapes module sould expose a "createBuilding" function');
-    });
-
-    test('Test objects created with the  createBuilding function', function() {
-      expect(3);
-      var building = window.Shapes.createBuilding(buildingAttr);
-      ok(typeof building.getArea === 'function', 'Object Created with "createBuilding" Should have a getArea function');
-      equal(building.getArea(),10000, 'Should return the area of the building computed from the set of points in the nodes attributes');
-    });
-
-
-    test('Test the createAmenity function', function() {
-      expect(1);
-      ok(typeof window.Shapes.createNatural !== 'undefined', 'The Shapes module sould expose a "createNatural" function');
-    });
-    test('Test objects created with the  createNatural function', function() {
-      expect(2);
-      var natural = window.Shapes.createNatural(naturalAttr);
-      ok(typeof natural.getType === 'function', 'Object Created with "createNatural" Should have a getType function');
-      equal(natural.getType(),'water', 'Should return the value corresponding to the "natural" property in the attributes');
-    });
-
-
-
-
-
-    /*-----------------------------------*/
-    /* PART THREE: Test with a JSON file */
-    /*-----------------------------------*/
-
-    // TODO Write the whole test module for testing with the app/data/eure.json file.
-
-    module('Asynchronous Unit Test Module', {
-        setup: function() {
-            stop();
-
-            // You can load a resource before loaching the test...
-            $.get('test.json').success(function(data){
-                obj = data;
-                start();
-            });
-
-            // ... Or any asynchroneous task
-            // window.setTimeout(function() {
-            //     obj = {a:'OK', b:'KO'};
-            //     start();
-            // }, 1000);
-
-        }
-    });
-
-    test('test 1', function() {
-        equal(obj.a, 'OK', 'Message');
-        equal(obj.a+'KO', 'OKKO', 'Message');
-    });
-
-    test('test 2', function() {
-        equal(obj.a+obj.b, 'OKKO', 'FAil');
-    });
 
 }());
